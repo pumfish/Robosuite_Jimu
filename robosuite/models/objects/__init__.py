ddf99558e7bd9acbe240f2ea5d9c8d8fe666b343
He Guanhua <59083036+pumfish@users.noreply.github.com>
2024-08-28 18:20:34 +0800

    Add files via upload

---
+++ robosuite/models/objects/__init__.py
@@ -0,0 +1,30 @@
+from .objects import MujocoObject, MujocoXMLObject, MujocoGeneratedObject
+from .generated_objects import CompositeBodyObject, CompositeObject, PrimitiveObject
+from .object_groups import ObjectGroup
+
+from .xml_objects import (
+    BottleObject,
+    CanObject,
+    LemonObject,
+    MilkObject,
+    BreadObject,
+    CerealObject,
+    SquareNutObject,
+    RoundNutObject,
+    MilkVisualObject,
+    BreadVisualObject,
+    CerealVisualObject,
+    CanVisualObject,
+    PlateWithHoleObject,
+    DoorObject,
+
+    DrawerObject,
+    XBoxVisualObject,
+    SBoxVisualObject,
+    SquareNutVisualObject,
+    RoundNutVisualObject,
+)
+from .primitive import *
+from .composite import *
+from .composite_body import *
+from .group import *
